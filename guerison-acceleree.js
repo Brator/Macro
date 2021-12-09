10d6fc56d79fa76a19eaf3c4dbf13e8192521d36
Brator <74437384+Brator@users.noreply.github.com>
2021-12-09 12:02:35 +0100

    Update guerison-acceleree.js

--- guerison-acceleree.js
+++ guerison-acceleree.js
@@ -1,11 +1,22 @@
-// guérison accélérée
-
+//// Guérison accelérée
+  ////// avec message 
 main()
 
 async function main(){
   let newHealth = actor.data.data.attributes.hp.temp + 1
 
-  //update the actor health
-  await actor.update({"data.attributes.hp.temp": newHealth});
-  ui.notifications.info(`${actor.data.name} guérit 1 pts de vie temporaire`)
+//mets à jour la santé temporaire
+await actor.update({"data.attributes.hp.temp": newHealth});
+ui.notifications.info(`${actor.data.name} guérit 1 pts de vie temporaire`)
+  
+// annonce dans le chat que le pion sélectionné guérit
+let controlledToken = canvas.tokens.controlled[0];
+const content = `<p>${controlledToken.name} guérit de 1 pv temporaire</p>`;
+
+ChatMessage.create({
+  speaker: ChatMessage.getSpeaker(controlledToken),
+  content: content,
+  type: CONST.CHAT_MESSAGE_TYPES.OTHER
+});
+
 }
