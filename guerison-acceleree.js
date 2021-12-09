e863c60ceb250aa414b27760ae0b5686d6404296
Brator <74437384+Brator@users.noreply.github.com>
2021-12-09 12:25:14 +0100

    Update guerison-acceleree.js

--- guerison-acceleree.js
+++ guerison-acceleree.js
@@ -1,15 +1,20 @@
-//// Guérison accelérée
-  ////// avec message 
+//// New Health is going to be grater the max health
+  ////// If so, we want the new health to max 
 main()
 
 async function main(){
   let newHealth = actor.data.data.attributes.hp.temp + 1
-
-//mets à jour la santé temporaire
-await actor.update({"data.attributes.hp.temp": newHealth});
-ui.notifications.info(`${actor.data.name} guérit 1 pts de vie temporaire`)
   
-// annonce dans le chat que le pion sélectionné guérit
+    //If token is max health if so, don't do anything
+  if(actor.data.data.attributes.hp.temp == 116){
+    ui.notifications.error(`${actor.data.name} a déjà le max de pv temporaire`);
+    return;
+  }
+
+  //update the actor health
+  await actor.update({"data.attributes.hp.temp": newHealth});
+  ui.notifications.info(`${actor.data.name} guérit 1 pts de vie temporaire`)
+
 let controlledToken = canvas.tokens.controlled[0];
 const content = `<p>${controlledToken.name} guérit de 1 pv temporaire</p>`;
 
